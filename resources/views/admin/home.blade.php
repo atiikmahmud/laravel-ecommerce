b01462c3a64712331d0ed972ddc4ed3d2142367c
atiikmahmud <atikmahmud.cse@gmail.com>
2023-01-03 00:38:07 +0600

    Admin template setup

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,3 +1,37 @@
-<x-app-layout>
-    <h1 style="text-align: center; margin-top: 100px;">Welcome to Admin Dashboard</h1>
-</x-app-layout>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Corona Admin</title>
+    
+    @include('admin.css')
+
+    <link rel="shortcut icon" href="admin/assets/images/favicon.png" />
+  </head>
+  <body>
+    <div class="container-scroller">
+      <!-- partial:partials/_sidebar.html -->
+      
+      @include('admin.sidebar')
+
+      <!-- partial -->
+      <div class="container-fluid page-body-wrapper">
+        <!-- partial:partials/_navbar.html -->
+        
+        @include('admin.header')
+
+        <!-- partial -->
+        
+        @include('admin.body')
+        
+        <!-- main-panel ends -->
+      </div>
+      <!-- page-body-wrapper ends -->
+    </div>
+
+    @include('admin.script')
+    
+  </body>
+</html>
